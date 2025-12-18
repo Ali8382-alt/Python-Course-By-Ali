9f47b03b914ea6578c3f721d1b2478c7945c66a0
Ali <aliiqbalansari2@gmail.com>
2025-12-18 13:27:36 +0500

    Chapter 1

---
+++ codeexecution.py
@@ -0,0 +1,9 @@
+name = "Ali Ansari"
+age1 = 25
+age2 = 40
+print("Real Value:" , age2)
+favSubject = "Maths"
+age2 = age1
+print("Changed Value:" , age2)
+
+
---
+++ userinput.py
@@ -0,0 +1,7 @@
+# Program to take input from the user 
+
+print("A sample program to understand input method in python")
+
+name= input("Enter your  name :")
+
+print("Your  name is :", name)
